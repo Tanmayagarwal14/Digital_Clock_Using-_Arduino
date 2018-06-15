929ba6e04f840fe8450d7a64330d670980edb763
Tanmay Agarwal <tanmayagarwal@iith.ac.in>
2018-06-15 16:10:48 +0530

    Code For Controlling Two Displays In a clock

--- Controlling_Two_Display-Clock/Controlling_Two_Display-Clock.ino
+++ Controlling_Two_Display-Clock/Controlling_Two_Display-Clock.ino
@@ -1,29 +1,59 @@
+///This code is for controlling the two 7-seg display //
+
+
+// Defining the Variables///
 int a,b,c,d,e,f,g,W,X,Y,Z,i,j,thispin;
 int A=0,B=0,C=0,D=0;
 int ledpins[]={2,3,4,5,6,7,8,9,10};
 int pinCount=9;
-int r0;
+int r0,r1;
+int low_pins[]={9,10};
+int cnt[2];
 unsigned int initialtime,elapsed;
 void showit(int x);
 
+
+// Setting up the Pins//
+// the setup function runs once when you press reset or power the board
 void setup()
 {
+
+  //initialize digital pin 2 to 10 as an output
+  
   for(thispin=0;thispin<=pinCount;thispin++)
   {
     pinMode(ledpins[thispin],OUTPUT);
   
   }
 }
+
+//Main Function///
+//LOOP FUNCTION RUNS OVER AND OVER AGAIN FOREVER// 
  void loop()
+ {
+ for(r1=0;r1<=5;r1++) // count 0 to 5 after every 10 seconds
  {
   for(r0=0;r0<=9;r0++)
   {
+    // number of milliseconds Since program Strated.
     initialtime=millis();
-
-    for(elapsed=0;elapsed<=1000;elapsed=millis()-initialtime)
+    elapsed = initialtime;
+    //ensuring delay of 1 Second
+    while (elapsed - initialtime<=1000)
     {
-      digitalWrite(9,HIGH);
-      showit(r0);
+      elapsed=millis();
+      cnt[0]=r0;
+      cnt[1]=r1;
+      //Multiplexing the Displays
+      for(j=0;j<2;j++)
+      {
+        digitalWrite(low_pins[j],LOW);
+        digitalWrite(low_pins[i],HIGH);
+
+        showit(cnt[i]);
+        delay(2);
+      }
+    }
       
     }
   }
@@ -40,16 +70,19 @@ void setup()
   D=x%2;
   x=x/2;
 
-  
- a=(A&&!B&&!C&&!D)||(!A&&C);
+  //===========LOGIC FOR THE DISPLAY=========
+  a=(A&&!B&&!C&&!D)||(!A&&C);
   b=(A&&!B&&C)||(!A&&B&&C);
   c=(!A&&B&&!C&&!D);
   d=(!A&&!B&&C)||(A&&!B&&!C)||(A&&B&&C);
   e=A||(!B&&C);
   f=(A&&!C&&!D)||(A&&B)||(B&&!C);
   g=(!B&&!C&&!D)||(A&&B&&C);
+
+
+  ///////// Writting the Values on the Seven Segment Display///
   
-   digitalWrite(2,a);
+  digitalWrite(2,a);
   digitalWrite(3,b);
   digitalWrite(4,c);
   digitalWrite(5,d);
